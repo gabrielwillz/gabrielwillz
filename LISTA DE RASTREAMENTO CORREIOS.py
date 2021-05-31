53c092db63edb78a0bc747e60b4ec82c19b6b6f9
Gabriel <79047629+gabrielwillz@users.noreply.github.com>
2021-05-31 19:38:04 -0300

    Add files via upload

---
+++ LISTA DE RASTREAMENTO CORREIOS.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    print('###############################')
+    print('#### LISTA DE RASTREAMENTO ###')
+    print('###############################')
+    print()
+
+    codigos = int(input('Coloque número de rastreamentos que quer fazer: '))
+    lista_rastreo = []
+
+    i = 1
+    while i <= codigos:
+        codigo_rastreo = str(input('Coloque o numero de rastreio: '))
+        lista_rastreo.append(codigo_rastreo)
+        i += 1
+
+    print('-------------------------------------')
+    print()
+    print(f'Numero de Codigos de RASTREAMENTOS: {codigos}')
+    print()
+    print('####### LISTA DE RASTREAMENTO ###########')
+    print()
+
+    for codigo in lista_rastreo:
+        req = requests.post(url='https://www2.correios.com.br/sistemas/rastreamento/ctrl/ctrlRastreamento.cfm?',
+                            data={'objetos': codigo})
+        soup = BeautifulSoup(req.text, 'html.parser')
+        ult_atu = soup.find(id="UltimoEvento").strong.text
+        data = soup.find(id="UltimoEvento").text.split()[-1]
+        print(f'Status: {ult_atu} - Data: {data} - CODIGO: {codigo}')
+        print()
+        print('-------------------------------------------------------------')
+        print()
+
+    option = int(input('Quer fazer outro lista de Rastreamento?\n 1. SIM\n 2. NÃO\n'))
+
+    if option == 1:
+        main()
+    else:
+        print('Saindo.......')
+
+
+if __name__ == '__main__':
+    main()
